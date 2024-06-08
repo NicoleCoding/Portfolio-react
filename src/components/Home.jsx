76bcdd72e3059a37747738bceb024e58d07239de
NicoleCoding <nicole251097@gmail.com>
2024-06-08 17:36:00 +0300

    Adjustments in nav and home styling: - Changed text color on hover in nav links - Adjusted width of the "skills"-list - Updated text color of h2 in home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,14 +3,14 @@ import { Link } from "wouter";
 export default function Home() {
     return (
         <section className="container mx-auto p-4 md:p-8">
-            <section id="intro" className="flex flex-col md:flex-row justify-between items-center">
-                <h2 className="font-bold text-4xl">Frontend developer with UX skills</h2>
-                <p className="p-3">Welcome to my portfolio! My name is Nicole and I am an aspiring frontend developer with UX skills. With a solid foundation in HTML, CSS and JavaScript, in combination with my UX
+            <section id="intro" className="flex flex-col md:flex-col justify-between items-center">
+                <h2 className="font-bold text-4xl text-teal-500">Frontend developer with UX skills</h2>
+                <p className="p-3 text-xl">Welcome to my portfolio! My name is Nicole and I am an aspiring frontend developer with UX skills. With a solid foundation in HTML, CSS and JavaScript, in combination with my UX
                     skills, I aim to create amazing user experiences. Always learning and keen to sharpen my expertise in various technologies, since this field is always evolving.
                 </p>
 
             </section>
-            <ul className="bg-gradient-to-r from-teal-200 to-teal-500 p-2 rounded-lg mt-5 w-60 flex flex-col items-center">
+            <ul className="bg-gradient-to-r from-teal-200 to-teal-500 p-2 rounded-lg mx-auto w-80 flex flex-col items-center text-center">
             <h2 className="font-bold text-2xl pt-5">Relevant skillset</h2>
                 <li>HTML5</li>
                 <li>CSS3</li>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,10 +5,10 @@ export default function Nav() {
     return (
         <nav className="flex flex-col md:flex-row items-center">
             <ul className="flex flex-col md:flex-row md:items-center justify-end">
-                <li className="mb-2 md:mb-0"><Link className="block py-2 px-4 text-white hover:bg-gray-700" href="/">Home</Link></li>
-                <li className="mb-2 md:mb-0"><Link className="block py-2 px-4 text-white hover:bg-gray-700"href="/projects">Projects</Link></li>
-                <li className="mb-2 md:mb-0"><Link className="block py-2 px-4 text-white hover:bg-gray-700" href="/about">About</Link></li>
-                <li className="mb-2 md:mb-0"><Link className="block py-2 px-4 text-white hover:bg-gray-700" href="/contact">Contact</Link></li>
+                <li className="mb-2 md:mb-0"><Link className="block py-2 px-4 text-white hover:bg-gray-700 hover:text-teal-400" href="/">Home</Link></li>
+                <li className="mb-2 md:mb-0"><Link className="block py-2 px-4 text-white hover:bg-gray-700 hover:text-teal-400" href="/projects">Projects</Link></li>
+                <li className="mb-2 md:mb-0"><Link className="block py-2 px-4 text-white hover:bg-gray-700 hover:text-teal-400" href="/about">About</Link></li>
+                <li className="mb-2 md:mb-0"><Link className="block py-2 px-4 text-white hover:bg-gray-700 hover:text-teal-400" href="/contact">Contact</Link></li>
             </ul>
     
     </nav>
