0f10383fcd5e2686dfe6006eeb66c4df3628fec8
NicoleCoding <nicole251097@gmail.com>
2024-06-10 16:06:54 +0300

    Created array for projects which will be shown in projects-section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,29 @@
 export default function Projects() {
+    const projects = [{
+        projectName: "",
+        projectDesc: "",
+
+    },
+    {
+        projectName: "",
+        projectDesc: "",
+
+    },
+    {
+        projectName: "",
+        projectDesc: "",
+    },
+    {
+        projectName: "",
+        projectDesc: "",
+    },
+    {
+        projectName: "",
+        projectDesc: "",
+    }
+];
+
+
     return (
         <section>
             <h2 className="font-bold">Projects</h2>
