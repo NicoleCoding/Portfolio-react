28a69cb888f4e45d3097bcb08025892a2f8b8785
NicoleCoding <nicole251097@gmail.com>
2024-06-19 12:34:22 +0300

    - Implemented project carousel which showcases each project - Styling for the project carousel - Added the project carousel inside the projects component

--- src/components/ProjectCarousel.jsx
+++ src/components/ProjectCarousel.jsx
@@ -1,11 +1,40 @@
 import Slider from "react-slick";
 import './ProjectCarousel.css';
 
+const projects = [{
+    id: "1",
+    title: "E-commerce application",
+    description: "",
+
+},
+{   
+    id: "2",
+    title: "Task management application",
+    description: "",
+
+},
+{   
+    id: "3",
+    title: "Hotelbooking application",
+    description: "",
+},
+{   
+    id: "4",
+    title: "Recipe application",
+    description: "",
+},
+{   
+    id: "5",
+    title: "",
+    description: "",
+}
+];
+
 function ProjectCarousel() {
     const settings = {
         dots: true,
         infinite: true,
-        speed: 500,
+        speed: 700,
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
@@ -16,7 +45,12 @@ function ProjectCarousel() {
     return (
         <div className="carousel">
             <Slider {...settings}>
-                
+                {projects.map(project => (
+                    <div key={project.id} className="project-slide">
+                        <h3>{project.title}</h3>
+                        <p>{project.description}</p>
+                    </div>
+                ))}
             </Slider>
         </div>
     )
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,50 +1,14 @@
+import ProjectCarousel from "./ProjectCarousel";
+
 export default function Projects() {
-    const projects = [{
-        id: "",
-        title: "",
-        description: "",
 
-    },
-    {   
-        id: "",
-        title: "",
-        description: "",
-
-    },
-    {   
-        id: "",
-        title: "",
-        description: "",
-    },
-    {   
-        id: "",
-        title: "",
-        description: "",
-    },
-    {   
-        id: "",
-        title: "",
-        description: "",
-    }
-];
-
-const renderProjects = () => {
-    return projects.map(project => (
-        <div key={project.id} className="project">
-            <h3>{project.title}</h3>
-            <p>{project.description}</p>
-        </div>
-    ));
-
-};
 
 
     return (
         <section id="projects-display">
             <h2 className="font-bold">Projects</h2>
-            <div id="projects">
-                {renderProjects()};
-            </div>
+            <ProjectCarousel />
+            
         </section>
         
 
