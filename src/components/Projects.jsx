4eb05af535963bebb38ce1a71ea7eefd5ecf5278
NicoleCoding <nicole251097@gmail.com>
2024-07-02 23:44:13 +0300

    Added a little rocket icon next to the projects heading

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,5 @@
 import ProjectCarousel from "./ProjectCarousel";
+import { LuRocket } from "react-icons/lu";
 
 export default function Projects() {
 
@@ -6,7 +7,10 @@ export default function Projects() {
 
     return (
         <section id="projects-display">
-            <h2 className="font-bold text-4xl text-teal-500">Projects</h2>
+            <div className="flex items-center justify-center">
+                <h2 className="font-bold text-4xl text-teal-500">Projects</h2>
+                <LuRocket className="text-teal-500 m-2"/>
+            </div>
             <ProjectCarousel />
             
         </section>
