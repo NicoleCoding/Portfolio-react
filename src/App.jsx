0265ce71bfd0ba40d7b5a742455fa2af5930a719
NicoleCoding <nicole251097@gmail.com>
2024-07-12 22:29:46 +0300

    Paths corrected in App.jsx for the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import "./App.css";
 import Header from "./components/Header";
-import Home from "./components/Home";
-import About from "./components/About";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 import Footer from "./components/Footer";
 import { Switch, Router, Route } from "wouter";
 
