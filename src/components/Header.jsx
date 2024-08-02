462f141381ec6681a9a665e01f0d38052d0dcdcd
NicoleCoding <nicole251097@gmail.com>
2024-08-02 17:21:09 +0300

    Header and nav background color changed, in addition to the text color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import Nav from "./Nav";
 export default function Header() {
 
     return (
-        <header className="bg-black sticky top-0 left-0 right-0 z-10 w-full">
+        <header className="bg-white sticky top-0 left-0 right-0 z-10 w-full">
             <div className="max-w-7xl mx-auto px-4 flex justify-between items-center py-3">
-            <h1 className="text-white">Le portfolio</h1>
+            <h1 className="text-black">Le portfolio</h1>
             <Nav />
             </div>
         </header>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,16 +7,16 @@ export default function Nav() {
         <nav className="flex flex-col md:flex-row items-center font-custom">
             <ul className="flex flex-col md:flex-row md:items-center justify-end">
                 <li className="mb-2 md:mb-0"><Link className={`block py-2 px-4 rounded-lg ${
-                            location === "/" ? "text-teal-500" : "text-white"
+                            location === "/" ? "text-teal-500" : "text-black"
                         } hover:bg-gray-700 hover:text-teal-400`} href="/">Home</Link></li>
                 <li className="mb-2 md:mb-0"><Link className={`block py-2 px-4 rounded-lg ${
-                            location === "/projects" ? "text-teal-500" : "text-white"
+                            location === "/projects" ? "text-teal-500" : "text-black"
                         } hover:bg-gray-700 hover:text-teal-400`} href="/projects">Projects</Link></li>
                 <li className="mb-2 md:mb-0"><Link className={`block py-2 px-4 rounded-lg ${
-                            location === "/about" ? "text-teal-500" : "text-white"
+                            location === "/about" ? "text-teal-500" : "text-black"
                         } hover:bg-gray-700 hover:text-teal-400`} href="/about">About</Link></li>
                 <li className="mb-2 md:mb-0"><Link className={`block py-2 px-4 rounded-lg ${
-                            location === "/contact" ? "text-teal-500" : "text-white"
+                            location === "/contact" ? "text-teal-500" : "text-black"
                         } hover:bg-gray-700 hover:text-teal-400`} href="/contact">Contact</Link></li>
             </ul>
     </nav>
