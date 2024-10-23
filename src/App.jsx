453eec2a22ef050ba144f91eb26f743fdc5b3632
NicoleCoding <nicole251097@gmail.com>
2024-10-23 11:34:30 +0300

    - Removed non-existant component from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
-import Blog from "./pages/Blog";
 import Footer from "./components/Footer";
 import { Switch, Router, Route } from "wouter";
 
@@ -19,7 +18,6 @@ function App() {
         <Switch>
           <Route path="/" component={Home}/>
           <Route path="/projects" component={Projects}/>
-          <Route path="/blog" component={Blog}/>
           <Route path="/about" component={About}/>
           <Route path="/contact" component={Contact}/>
         </Switch>
