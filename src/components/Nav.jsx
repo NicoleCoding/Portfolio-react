b8386e130373cd747cb6afbe89db756f5440f059
NicoleCoding <nicole251097@gmail.com>
2024-10-24 16:15:29 +0300

    - Implemented hamburger menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,17 @@
 import { Link, useLocation } from "wouter";
+import Hamburger from "hamburger-react";
+import { useState } from "react";
 
 export default function Nav() {
     const [location] = useLocation();
+    const [isOpen, setOpen] = useState(false);
 
     return (
         <nav className="flex flex-col md:flex-row items-center font-custom">
-            <ul className="flex flex-col md:flex-row md:items-center justify-end">
+            <div className="md:hidden">
+                <Hamburger toggled={isOpen} toggle={setOpen}/>
+            </div>
+            <ul className={`flex flex-col md:flex-row md:items-center justify-end transition-transform duration-300 ${isOpen ? "block" : "hidden"} md:block`}>
                 <li className="mb-2 md:mb-0"><Link className={`block py-2 px-4 rounded-lg ${
                             location === "/" ? "text-teal-500" : "text-black"
                         } hover:text-teal-400`} href="/">Home</Link></li>
