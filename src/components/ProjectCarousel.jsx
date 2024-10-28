16152846b93e64258f6bb56bd559fd2fd4c0ba4c
NicoleCoding <nicole251097@gmail.com>
2024-10-28 09:56:13 +0200

    Another attempt to fix the image issue (not displaying in the project carousel)

--- src/components/ProjectCarousel.jsx
+++ src/components/ProjectCarousel.jsx
@@ -6,17 +6,17 @@ import '../custom-carousel.css';
 const projects = [{
     id: "1",
     title: "Hotel booking application - Palm Booking Agency",
-    projectImage: "public/prototype-hotel-app.PNG"
+    projectImage: "/public/prototype-hotel-app.PNG"
 },
 {   
     id: "2",
     title: "Informative application about climate change - KlimatKoll",
-    projectImage: "public/prototype-climate-app.PNG"
+    projectImage: "/public/prototype-climate-app.PNG"
 },
 {
     id: "3",
     title: "Library App - BookVault",
-    projectImage: "public/library-app-img.PNG"
+    projectImage: "/public/library-app-img.PNG"
 
 }
 ];
